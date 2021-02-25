d1a18356f1803b2d17ef542d5e66159e9de7bc43
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-02-25 15:36:59 -0600

    Update MoreFuncsStudio02.js
    Completed the bonus to add recursion to my minimumRecursion function.

--- MoreFuncsStudio02.js
+++ MoreFuncsStudio02.js
@@ -3,26 +3,51 @@ let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
-function findMinimumNum(arr){
-  let min = arr[0];
-  for (i = 0; i < arr.length; i++){
-    if (arr[i] < min){
-      min = arr[i];
+// Original function
+// function findMinimumNum(arr){
+//   let min = arr[0];
+//   for (i = 0; i < arr.length; i++){
+//     if (arr[i] < min){
+//       min = arr[i];
+//     }
+//   }
+//   return min;
+// }
+
+// Recursion function
+function minimumRecursion (arr) {
+  if (arr.length <= 1) {
+    return arr[0];
+  } else {
+    if (arr[0] > arr[1]) {
+      arr = arr.slice(1);
+    } else {
+      arr = [arr[0]].concat(arr.slice(2));
     }
+    return minimumRecursion(arr);
   }
-  return min;
 }
-// console.log(findMinimumNum(nums1));
-// console.log(findMinimumNum(nums2));
-// console.log(findMinimumNum(nums3));
+
 
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
 
+// Without recursion
 //Your function here...
+// function sortIncreasingArr(arr) {
+//   let sortedArr = [];
+//   while (arr.length > 0) {
+//     let minimum = findMinimumNum(arr);
+//     sortedArr.push(minimum);
+//     arr.splice(arr.indexOf(minimum), 1);
+//   }
+//   return sortedArr;
+// }
+
+// Using recursion
 function sortIncreasingArr(arr) {
   let sortedArr = [];
   while (arr.length > 0) {
-    let minimum = findMinimumNum(arr);
+    let minimum = minimumRecursion(arr);
     sortedArr.push(minimum);
     arr.splice(arr.indexOf(minimum), 1);
   }
